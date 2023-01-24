3c96e7a07cbda613e44406a29db14707ef82e07b
Onno van der Zee <o.vanderzee@eaglescience.nl>
2023-01-25 00:04:06 +0100

    feature tests

--- src/test.ts
+++ src/test.ts
@@ -20,3 +20,30 @@ test('simple usage', () => {
 
     assert(report.length === 3, 'after using 3 times, the should be 3 results')
 })
+
+test('report; returns call parameters', () => {
+    const testSpion: Spion = createSpion(subject, 'pureAddition', null)
+    subject.pureAddition(3, 7)
+    const report: CallInfo[] = testSpion.report()
+
+    assert(report[0].args[0] === 3, `first argument should be 3, was: ${report[0].args[0]}`)
+    assert(report[0].args[1] === 7, `last argument should be 7, was: ${report[0].args[1]}`)
+    assert(report[0].return === 10, `return value should be 10, was: ${report[0].return}`)
+})
+
+test('lifecycle; report() restores the original function', () => {
+    const referenceOriginal = subject.pureAddition
+
+    const testSpion: Spion = createSpion(subject, 'pureAddition', null)
+    const referenceClone = subject.pureAddition
+
+    assert(referenceOriginal !== referenceClone, 'clone should not be the original')
+
+    testSpion.report()
+    const referenceAfterwards = subject.pureAddition
+
+    assert(referenceOriginal === referenceAfterwards, 'original should be restored')
+    assert(referenceAfterwards !== referenceClone, 'clone should be removed from object')
+    assert(referenceOriginal !== referenceClone, 'clone should not be the original after restore')
+})
+
